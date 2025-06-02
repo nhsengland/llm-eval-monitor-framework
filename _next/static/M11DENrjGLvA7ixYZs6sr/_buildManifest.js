cb43b643f55f381e338e268599040c734d825db3
danjscho <67116171+danjscho@users.noreply.github.com>
2025-06-02 10:44:50 +0000

    Deploying to gh-pages from @ nhsengland/llm-eval-monitor-framework@8fa977a6733a1edb8c154a88457288906e0e2b8b 🚀

--- _next/static/M11DENrjGLvA7ixYZs6sr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/727-fa76cee590969ab7.js","static/chunks/pages/index-49e19f8c4c260b88.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
