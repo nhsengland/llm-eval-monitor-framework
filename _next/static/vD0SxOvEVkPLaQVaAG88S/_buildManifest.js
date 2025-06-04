3908d9be907bfe2cf153879f6bb90d1dfbfa63ef
harrietrs <harrietrsands@gmail.com>
2025-06-04 10:37:59 +0000

    Deploying to gh-pages from @ nhsengland/llm-eval-monitor-framework@c3275a4a16cf6b80c54c7149146f7b07c7d01299 🚀

---
+++ _next/static/vD0SxOvEVkPLaQVaAG88S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/877-7bace8e325c7d237.js","static/chunks/pages/index-a93da756bc1ea40e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
