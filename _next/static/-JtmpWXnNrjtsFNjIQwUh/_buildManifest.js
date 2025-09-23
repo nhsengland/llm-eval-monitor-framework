4015263a94f10a9ca3b49eead7b31908390daf0a
amaiaita <114224821+amaiaita@users.noreply.github.com>
2025-09-23 07:53:36 +0000

    Deploying to gh-pages from @ nhsengland/llm-eval-monitor-framework@54e750a25944b4973740550fc38378b672d706db 🚀

---
+++ _next/static/-JtmpWXnNrjtsFNjIQwUh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/877-7bace8e325c7d237.js","static/chunks/pages/index-44126e0540dac9bf.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/QXOzgHYJPgWyxlcyfRLuo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/877-7bace8e325c7d237.js","static/chunks/pages/index-216c28d3d8cd601a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
